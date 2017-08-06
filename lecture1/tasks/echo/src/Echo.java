c2dd9942fe818807e49e3a05069838b56da478f8
luxmeter <superhans@ludete.de>
2017-08-06 10:57:48 +0200

    removed array list and updated loop

--- lecture1/tasks/echo/src/Echo.java
+++ lecture1/tasks/echo/src/Echo.java
@@ -9,10 +9,9 @@ public class Echo {
     private static final String DELIMITER = " ";
 
     public static void main(String[] args) {
-        List<String> arguments = Arrays.asList(args);
         String concatenateArguments = EMPTY_STRING;
-        for (String argument : arguments) {
-            concatenateArguments = concatenateArguments + argument + DELIMITER;
+        for (int i = 0; i < args.length; i++) {
+            concatenateArguments = concatenateArguments + args[i] + DELIMITER;
         }
         // removing tailing space before printing
         System.out.println(concatenateArguments.trim());
